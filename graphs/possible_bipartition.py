4325ab2733e5dcc755eaa0fd2718f08affa1a643
Joanna <joannaparis7@gmail.com>
2023-01-19 13:16:22 -0800

    completed

--- graphs/possible_bipartition.py
+++ graphs/possible_bipartition.py
@@ -5,8 +5,41 @@ def possible_bipartition(dislikes):
     """ Will return True or False if the given graph
         can be bipartitioned without neighboring nodes put
         into the same partition.
-        Time Complexity: ?
+        Time Complexity: o()
         Space Complexity: ?
     """
-    pass
+    partition_1 = set()
+    partition_2 = set()
+    q = deque(dislikes)
+
+    for i in range(len(q)):
+        if q[i] in partition_1 or q[i] in partition_2:
+            continue
+        queue = deque()
+        queue.append(q[i])
+        partition = "1"
+        while queue:
+            for i in range(len(queue)):
+                dog = queue.popleft()
+                print(dog, i)
+                if partition == "1":
+                    if dog in partition_2:
+                        return False
+                    partition_1.add(dog)
+
+                if partition == "2":
+                    if dog in partition_1:
+                        return False
+                    partition_2.add(dog)
+
+                for dislike in dislikes[dog]:
+                    if dislike not in partition_1 and dislike not in partition_2:
+                        queue.append(dislike)
+
+            if partition == "1":
+                partition = "2"
+            else:
+                partition = "1"
+
+    return True
 
